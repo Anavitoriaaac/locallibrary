7d8f932c4232a82eae66884519d0ac3e1754287a
anavitoriakl <andrade.vitoria@escolar.ifrn.edu.br>
2023-09-14 21:48:04 -0300

    PARTE 5 TUTORIAL
    forem feitas alterações para realizar a parte 5

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -128,4 +128,21 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
